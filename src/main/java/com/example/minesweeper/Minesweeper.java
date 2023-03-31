b65449d704d716dba38c9d02caecf9cbb16746de
TheKrigerenJp <126723794+TheKrigerenJp@users.noreply.github.com>
2023-03-30 22:59:33 -0600

    Update Minesweeper.java
    Este ya es el proyecto nuevo, con ventana principal y cambiando el nombre de la clase mina a casilla

--- src/main/java/com/example/minesweeper/Minesweeper.java
+++ src/main/java/com/example/minesweeper/Minesweeper.java
@@ -1,68 +1,59 @@
-package com.example.minesweeper;
+package com.example.minesweeper3;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Minesweeper extends Application {
+    private static final int filas = 8;
+    private static final int columnas = 8;
+    private static final int minas = 10;
+    //private Casilla[][] tablero = new Casilla[filas][columnas];
+    private int minas_rest = minas;
+    private int casilla_rest = filas * columnas - minas;
 
-    static int columnas = 8;
-    static int filas = 8;
-    static int num_minas = 5;
-    //static mina casilla[][] = new mina[][];
-    static GridPane root = new GridPane();
-    static Stage escenario_guard;
 
+    @Override
     public void start(Stage stage) {
-        escenario_guard = stage;
-        stage.setTitle("App "+ this.getClass().getSimpleName());
-        //Layout que nos permita crear la matriz
-        root.setHgap(1);
-        root.setVgap(1);
-        root.setPadding(new Insets(15,15,15,15));
+        stage.setTitle("Menú");
 
-        jugarPartida();
-
-        //Se crea la escena y se asigna el escenario
-        Scene escena = new Scene(root);
-        stage.setScene(escena);
-        stage.sizeToScene();
-        stage.show();
-    }
-    public static void repetirPartida(Stage escenario){
-        //Creamos otro layout para crear la nueva partida
-        root = new GridPane();
-        root.setHgap(1);
-        root.setVgap(1);
-        root.setPadding(new Insets(15,15,15,15));
-
-        jugarPartida();
-
-        //igualmente se crea un escenario y se le asigna un escenario
-        Scene escena = new Scene(root);
-        escenario_guard.setScene(escena);
-        escenario_guard.sizeToScene();
-        escenario_guard.show();
-
-    }
-    public static void jugarPartida(){
-        //solicitud de datos
-        //solicitud_Datos();
-        //Se inserta los botones utilizando un for
-        for (int i=0; i<columnas; i++){
-            for (int j = 0; j<filas; j++){
-
+        Button dummy_Button = new Button("Modo Dummy");
+        dummy_Button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //Aqui va juego en modo Dummy
             }
-        }
-    }
+        });
 
+        Button advance_Button = new Button("Modo Advance");
+        advance_Button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                //Codigo del juego en Advance
+            }
+        });
+
+        VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(dummy_Button, advance_Button);
+
+        //Creando escena
+        Scene scene = new Scene(vBox, 400, 500);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
+
 }
