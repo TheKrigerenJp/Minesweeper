21cbe140e01c3861d910ad5bda3e5e8aed78db89
TheKrigerenJp <126723794+TheKrigerenJp@users.noreply.github.com>
2023-04-18 23:29:57 -0600

    Ultimo commit

--- src/main/java/com/example/Tablero.java
+++ src/main/java/com/example/Tablero.java
@@ -3,42 +3,67 @@ package com.example.minesweeper3;
 import java.util.Random;
 
 
-
+/**
+ *
+ */
 public class Tablero {
     public Casilla[][] casillas;
 
     //Tablero tablero = new Tablero(8, 8, 10);
 
     private int numMinas = 8;
-    private int numCasilla_tapada;
-    private int numCasilla_faltantes;
-    private boolean game_over;
-    private boolean victoria;
+    //private int numCasilla_tapada;
+    //private int numCasilla_faltantes;
+    //private boolean game_over;
+    //private boolean victoria;
 
     private int filas = 8;
     private int columnas = 8;
 
+    /**
+     *
+     * @param numFilas
+     * @param numColumnas
+     */
     public Tablero(int numFilas, int numColumnas){
         this.casillas = new Casilla[numFilas][numColumnas];
-        this.numCasilla_tapada = numFilas * numColumnas;
-        this.numCasilla_faltantes = numFilas * numColumnas - numMinas;
-        this.game_over = false;
-        this.victoria = false;
+        //this.numCasilla_tapada = numFilas * numColumnas;
+        //this.numCasilla_faltantes = numFilas * numColumnas - numMinas;
+        //this.game_over = false;
+        //this.victoria = false;
 
     }
 
+    /**
+     *
+     * @return
+     */
     public int getFilas(){
         return filas;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getColummnas(){
         return columnas;
     }
 
+    /**
+     *
+     * @param fila
+     * @param columna
+     * @return
+     */
     public Casilla getCasilla(int fila, int columna){
         return casillas[fila][columna];
     }
 
+    /**
+     * Se crea las casillas y se llama al metodo que coloca minas
+     *
+     */
     public void crear_casillas(){
 
         //creamos las casillas
@@ -52,7 +77,10 @@ public class Tablero {
         colocarMinas();
     }
 
-
+    /**
+     * Se coloca de manera aleatoria dentro de los limites del gridpane la cantidad de minas
+     *
+     */
 
     private void colocarMinas(){
         int minas_colo = 0;
@@ -62,8 +90,10 @@ public class Tablero {
 
             int fila = (int)(Math.random()*casillas.length);
             int columna = (int)(Math.random()*casillas[0].length);
+            System.out.println(columna+"" +","+ fila+"");
 
             if (!this.casillas[fila][columna].hay_Mina()){
+
                 casillas[fila][columna].colocarBomba();
                 minas_colo++;
             }else {
@@ -72,19 +102,9 @@ public class Tablero {
 
         }
 
-        //Se actualiza minas adyacentes
-        /*for (int fila = 0; fila < filas; fila++){
-            for (int columna = 0; columna < columnas; columna++){
-                if (tablero[fila][columna].esMina){
-                    for (Casilla vecinito : getVecinos(tablero[fila][columna])){
-                        vecinito.aumenta_minasCercanas();
-                    }
-                }
-            }
-        }*/
     }
 
-    public int getNumeroMinasCercanas (Casilla casilla){
+    /*public int getNumeroMinasCercanas (Casilla casilla){
 
         int fila = casilla.getFila();
         int columna = casilla.getColumna();
@@ -101,30 +121,16 @@ public class Tablero {
             }
         }
         return num_minas;
-    }
-
-    /*private void descubrirCasilla(Casilla casilla) {
-        casilla.setMina_oculta(false);
-        if (!casilla.mina_oculta) {
-            return;
-        }
-
-        casilla.descubrir();
-        numCasilla_faltantes--;
-
-        if (casilla.hay_Mina()) {
-            minas_rest--;
-        }
-
-        if (casilla.getNumeroMinasCercanas == 0) {
-            for (Casilla vecino : getVecinos(casilla)) {
-                descubrirCasilla(vecino);
-            }
-        }
     }*/
 
+    /**
+     * Este metodo es el que genera el efecto "cascada" al encontrar varias casillas seguidas sin minas alrededor, descubrirá esas casillas
+     * @param fila
+     * @param columna
+     */
 
     public void revelar_tablero(int fila, int columna){
+        System.out.println("Entra en revelar");
         if (fila<0 || columna<0 || fila >= getFilas() || columna >= getColummnas()){
             return;
         }
@@ -134,13 +140,16 @@ public class Tablero {
 
         if (casillas[fila][columna].getMinas_Adyacentes() != 0){
             casillas[fila][columna].setText(casillas[fila][columna].getMinas_Adyacentes()+"");
+            casillas[fila][columna].setStyle("-fx-background-color: red");
             casillas[fila][columna].abrirCasilla();
             casillas[fila][columna].setDisable(true);
+            casillas[fila][columna].setStyle("-fx-background-color: darkred");
             return;
         }
 
         casillas[fila][columna].abrirCasilla();
         casillas[fila][columna].setDisable(true);
+        casillas[fila][columna].setStyle("-fx-background-color: red");
 
         for (int i = fila-1; i<=fila+1; i++){
             for(int j = columna-1; j<=columna+1; j++){
@@ -154,9 +163,13 @@ public class Tablero {
 
     }
 
-
-
-    public boolean pos(int i ,int j){
+    /**
+     *
+     * @param i
+     * @param j
+     * @return
+     */
+    public boolean posi(int i ,int j){
         if(i>=0&&j>=0&&i<=7&&j<=7){
             return true;
         }else {
@@ -164,61 +177,57 @@ public class Tablero {
         }
     }
 
-
     /**
-     * Se generan los numeros de minas
-     * adyacentes y se agregan a la variable
-     * numrev en la matriz de valores. Esto
-     * se hace para todos los espacios en la matriz.
+     *
      */
-    public void contarMinasAdyacentes() { // genera los numeros de minas adyacentes y los añade a matrizvalores.numrev
+    public void contarMinasAdyacentes() { 
         for (int i=0; i<=7; i++) {
-            for (int j=0; j<=7; j++) { // indices para recorrer la matriz
+            for (int j=0; j<=7; j++) { 
                 int contador = 0;
-                if (!casillas[i][j].hay_Mina()) { // si no hay una mina en el espacio
-                    if (pos(i - 1, j - 1)) { // si el espacio de arriba a la izquierda no se sale del arreglo
-                        if (casillas[i - 1][j - 1].hay_Mina()) { // si el espacio de arriba a la izquierda es una mina
-                            contador++; // se aumenta el contador
+                if (!casillas[i][j].hay_Mina()) { 
+                    if (posi(i - 1, j - 1)) { 
+                        if (casillas[i - 1][j - 1].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i - 1, j)) { // si el espacio de arriba no se sale del arreglo
-                        if (casillas[i - 1][j].hay_Mina()) { // si el espacio de arriba es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i - 1, j)) { 
+                        if (casillas[i - 1][j].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i - 1, j + 1)) { // si el espacio de arriba a la derecha no se sale del arreglo
-                        if (casillas[i - 1][j + 1].hay_Mina()) { // si el espacio de arriba a la derecha es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i - 1, j + 1)) { 
+                        if (casillas[i - 1][j + 1].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i, j - 1)) { // si el espacio de la izquierda no se sale del arreglo
-                        if (casillas[i][j - 1].hay_Mina()) { // si el espacio de la izquierda es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i, j - 1)) { 
+                        if (casillas[i][j - 1].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i, j + 1)) { // si el espacio de la derecha no se sale del arreglo
-                        if (casillas[i][j + 1].hay_Mina()) { // si el espacio de la derecha es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i, j + 1)) { 
+                        if (casillas[i][j + 1].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i + 1, j - 1)) { // si el espacio de abajo a la izquierda no se sale del arreglo
-                        if (casillas[i + 1][j - 1].hay_Mina()) { // si el espacio de abajo a la izquierda es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i + 1, j - 1)) { 
+                        if (casillas[i + 1][j - 1].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i + 1, j)) { // si el espacio de abajo no se sale del arreglo
-                        if (casillas[i + 1][j].hay_Mina()) { // si el espacio de abajo es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i + 1, j)) { 
+                        if (casillas[i + 1][j].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    if (pos(i + 1, j + 1)) { // si el espacio de abajo a la derecha no se sale del arreglo
-                        if (casillas[i + 1][j + 1].hay_Mina()) { // si el espacio de abajo a la derecha es una mina
-                            contador++; // se aumenta el contador
+                    if (posi(i + 1, j + 1)) { 
+                        if (casillas[i + 1][j + 1].hay_Mina()) { 
+                            contador++; 
                         }
                     }
-                    casillas[i][j].setMinas_Adyacentes(contador); // se asigna el contador a la variable numrev en la matriz de valores
-                } else { // si el espacio es una mina
-                    casillas[i][j].setMinas_Adyacentes(-1); // se le asigna un -1 para representar que hay una mina
+                    casillas[i][j].setMinas_Adyacentes(contador); 
+                } else { 
+                    casillas[i][j].setMinas_Adyacentes(-1); 
                 }
             }    }
     }
@@ -226,3 +235,4 @@ public class Tablero {
 
 
 }
+
